82b57e72cef137867fe6f21a5f0f49d9717b218c
Tsukasa Muto <73716890+tsukasamuto55@users.noreply.github.com>
2023-12-28 05:42:59 -0700

    Reactor the pivot helper function

--- implementation-and-practice/quick_sort.js
+++ implementation-and-practice/quick_sort.js
@@ -1,36 +1,25 @@
-// function pivotHelper(arr, start = 0, end = arr.length - 1) {
-//     let randomIndex = Math.floor(Math.random() * (arr.length))
-//     let pivot = arr[randomIndex];
-//     let swapIndex = start;
-//     console.log(pivot)
-//     for (let i = start; i <= end; i++) {
-//         if (pivot > arr[i]) {
-//             swapIndex++;
-//             swap(arr, swapIndex, i)
-//         }
-//     }
-//     swap(arr, start, swapIndex)
-//     console.log(arr)
-//     return swapIndex
-// }
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivotHelper(arr, left, right)
+        quickSort(arr, left, pivotIndex - 1)
+        quickSort(arr, pivotIndex + 1, right)
+    }
+    return arr
+}
 
 function pivotHelper(arr, start = 0, end = arr.length - 1) {
     // Selecting a pivot within the subarray range
     let randomIndex = start + Math.floor(Math.random() * (end - start + 1));
     let pivot = arr[randomIndex];
-
     // Swapping the pivot with the start element
     swap(arr, start, randomIndex);
-
     let swapIndex = start;
-
     for (let i = start + 1; i <= end; i++) {
         if (arr[i] < pivot) {
             swapIndex++;
             swap(arr, swapIndex, i);
         }
     }
-
     // Moving pivot to its correct position
     swap(arr, start, swapIndex);
     console.log(pivot, arr)
@@ -43,6 +32,4 @@ function swap(arr, idx1, idx2) {
     arr[idx2] = temp;
 }
 
-const result = pivotHelper([22, 5, 1, 3, 10, 9, 6, 44])
-
-console.log(result)
+console.log(quickSort([22, 5, 1, 3, 10, 9, 6, 44]))
